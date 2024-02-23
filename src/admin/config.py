b56d4a20f91b84b71d987372ac7e053d51427c65
Josué Luiz Barbosa e Silva <104951932+josuelb@users.noreply.github.com>
2024-02-23 02:30:49 -0300

    Config update
    Implementação de requsitos para conexão com o banco de dados

--- src/admin/config.py
+++ src/admin/config.py
@@ -1,2 +1,8 @@
-class AdminConfig:
-    pass
+from flask import Flask
+from flask_mysqldb import MySQL
+
+app = Flask(__name__)
+
+# Required
+app.config["MYSQL_USER"] = "root"
+app.config["MSQL_PASSWORD"] = ""
--- src/admin/update_db.py
+++ src/admin/update_db.py
@@ -3,3 +3,11 @@ Esse modulo vai ser responsavel por ser o intermediario entre banco de dados, in
 
 Ele vai atualizar, resgatar e gernciar informações do banco de dados.
 """
+
+class DataBase:
+    @staticmethod
+    def get():
+        pass
+    @staticmethod
+    def update():
+        pass
