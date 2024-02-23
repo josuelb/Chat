80eddf1d47993a36fb72eba27fd4f5636057a22d
Josué Luiz Barbosa e Silva <104951932+josuelb@users.noreply.github.com>
2024-02-23 01:47:19 -0300

    Update users.py
    Métodos de getters e setters e de nome de usuario, numero, contatos. Adicionado __iter__ para iterações.

--- src/db/users.py
+++ src/db/users.py
@@ -17,10 +17,10 @@ class Usuarios(Pessoa):
             self.__nameUser = nameuser
             self.__numero = numero
     
-    def __init__(self, nome, sobrenome, dataNasc, numeroCell, usuario=None):
+    def __init__(self, nome, sobrenome, dataNasc, numeroCell: int, usuario=None):
         super().__init__(nome, sobrenome, dataNasc)
         self.__usuario = usuario if usuario is not None else nome
-        self.__numeroCell = numeroCell
+        self.__numeroCell:int = numeroCell
 
         self.__contatos = []
     
@@ -30,12 +30,40 @@ class Usuarios(Pessoa):
     def __len__(self):
         return len(self.__contatos)
     
-    def __setitem__(self, item: dict):
-        for key, value in item:
-            if self.__
-    
-    def __setcontato(self, numeroCell, nameuser):
-        novoContato = self.Contatos(nameuser, numero=numeroCell)
+    def __iter__(self):
+        for contato in self.__contatos:
+            yield contato
 
-        self.__contatos.append(novoContato)
+    def getUser(self):
+        return self.__usuario
+    
+    def getNum(self):
+        return self.__numeroCell
+
+    def setUser(self, name):
+        if self.__usuario != name:
+            self.__usuario = name
+            return
+        raise ValueError
+    
+    def getcontato(self, num: int= None, user=None):
+        if num is not None:
+            for i in self:
+                if i.__numero == num:
+                    return i
+            return -1
+        
+        for value in self:
+            if value.__nameUser == user or value.__numero == num:
+                return value
+        return -1
+    
+    def setcontato(self, numeroCell: int, nameuser):
+        if self.getcontato(numeroCell) == -1:
+            novoContato = self.Contatos(nameuser, numero=numeroCell)
+
+            self.__contatos.append(novoContato)
+            return
+        
+        raise IndexError
 
